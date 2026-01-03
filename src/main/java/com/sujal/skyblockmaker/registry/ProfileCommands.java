510ed5223431cfe90bf0c20e81243a01d1f81761
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-03 14:29:02 +0530

    Create ProfileCommands.java

---
+++ src/main/java/com/sujal/skyblockmaker/registry/ProfileCommands.java
@@ -0,0 +1,41 @@
+package com.sujal.skyblockmaker.registry;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.DoubleArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.sujal.skyblockmaker.api.SkyblockProfileApi;
+import com.sujal.skyblockmaker.api.SkyblockStatsApi;
+import com.sujal.skyblockmaker.api.SkyblockStatHandler;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+
+public class ProfileCommands {
+
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+        
+        // 1. /sbadmin set <stat> <value> (Admin Only)
+        dispatcher.register(CommandManager.literal("sbadmin")
+            .requires(source -> source.hasPermissionLevel(2)) // OP check
+            .then(CommandManager.literal("set")
+                .then(CommandManager.argument("stat", StringArgumentType.word())
+                .then(CommandManager.argument("value", DoubleArgumentType.doubleArg())
+                .executes(context -> {
+                    String statName = StringArgumentType.getString(context, "stat");
+                    double val = DoubleArgumentType.getDouble(context, "value");
+                    ServerPlayerEntity player = context.getSource().getPlayer();
+
+                    try {
+                        SkyblockStatsApi.StatType type = SkyblockStatsApi.StatType.valueOf(statName.toUpperCase());
+                        SkyblockProfileApi.setBaseStat(player, type, val);
+                        SkyblockStatHandler.updatePlayerStats(player); // Immediate update
+                        context.getSource().sendFeedback(Text.literal("Updated Base " + statName + " to " + val), false);
+                    } catch (Exception e) {
+                        context.getSource().sendError(Text.literal("Invalid Stat Name! Use: HEALTH, STRENGTH, DEFENSE, etc."));
+                    }
+                    return 1;
+                })))));
+    }
+}
