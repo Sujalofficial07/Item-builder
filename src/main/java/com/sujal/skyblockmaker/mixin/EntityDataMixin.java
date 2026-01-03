397157bccbe240b2962eb7362d51379053e4e651
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-03 14:26:46 +0530

    Create EntityDataMixin.java

---
+++ src/main/java/com/sujal/skyblockmaker/mixin/EntityDataMixin.java
@@ -0,0 +1,36 @@
+package com.sujal.skyblockmaker.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NbtCompound;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public abstract class EntityDataMixin implements IEntityDataSaver {
+    private NbtCompound persistentData;
+
+    @Override
+    public NbtCompound getPersistentData() {
+        if(this.persistentData == null) {
+            this.persistentData = new NbtCompound();
+        }
+        return this.persistentData;
+    }
+
+    @Inject(method = "writeNbt", at = @At("HEAD"))
+    protected void injectWriteMethod(NbtCompound nbt, CallbackInfoReturnable info) {
+        if(persistentData != null) {
+            nbt.put("SkyblockMakerData", persistentData);
+        }
+    }
+
+    @Inject(method = "readNbt", at = @At("HEAD"))
+    protected void injectReadMethod(NbtCompound nbt, CallbackInfo info) {
+        if(nbt.contains("SkyblockMakerData")) {
+            persistentData = nbt.getCompound("SkyblockMakerData");
+        }
+    }
+}
