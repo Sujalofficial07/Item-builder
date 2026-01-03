419620d11614421b7151a0547e9694816d99fddc
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-03 11:32:26 +0530

    Create SkyblockStatsApi.java

---
+++ src/main/java/com/sujal/skyblockmaker/api/SkyblockStatsApi.java
@@ -0,0 +1,8 @@
+    public enum StatType {
+        STRENGTH, 
+        DAMAGE, 
+        CRIT_CHANCE, 
+        HEALTH,    // Make sure ye hai
+        DEFENSE,   // Make sure ye hai
+        INTELLIGENCE // Make sure ye hai
+    }
