cb6a4968b19ca060491383b0515127203d1fc13f
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-03 10:17:30 +0530

    Update SkyblockMakerClient.java

--- src/main/java/com/sujal/skyblockmaker/SkyblockMakerClient.java
+++ src/main/java/com/sujal/skyblockmaker/SkyblockMakerClient.java
@@ -3,8 +3,8 @@ package com.sujal.skyblockmaker;
 import com.sujal.skyblockmaker.api.SkyblockStatsApi;
 import com.sujal.skyblockmaker.client.gui.ItemBuilderScreen;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
@@ -15,18 +15,25 @@ public class SkyblockMakerClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         
-        // 1. Command Registration: /sbbuilder
+        // 1. Command Register (Client Side Only)
+        registerCommands();
+
+        // 2. Tooltip Register
+        registerTooltips();
+    }
+
+    private void registerCommands() {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
             dispatcher.register(ClientCommandManager.literal("sbbuilder")
                 .executes(context -> {
-                    // Command run hone par GUI open karo
-                    // Hum 'context.getSource().getClient()' use kar sakte hain ya MinecraftClient.getInstance()
+                    // Ye line server pe run hui toh crash karti hai, isliye ise Client file mein rakha hai
                     MinecraftClient.getInstance().setScreen(new ItemBuilderScreen());
                     return 1;
                 }));
         });
+    }
 
-        // 2. Tooltip logic (Stats dikhana)
+    private void registerTooltips() {
         ItemTooltipCallback.EVENT.register((stack, context, lines) -> {
             if (stack.hasNbt() && stack.getNbt().contains(SkyblockStatsApi.NBT_KEY)) {
                 
