e04ad86a4c300e24f0379c7a6f20802705501772
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-03 14:29:36 +0530

    Update SkyblockMakerMod.java

--- src/main/java/com/sujal/skyblockmaker/SkyblockMakerMod.java
+++ src/main/java/com/sujal/skyblockmaker/SkyblockMakerMod.java
@@ -1,27 +1,25 @@
 package com.sujal.skyblockmaker;
 
-import com.sujal.skyblockmaker.api.SkyblockItemDefaults;
+import com.sujal.skyblockmaker.api.SkyblockStatHandler;
 import com.sujal.skyblockmaker.registry.ModPackets;
+import com.sujal.skyblockmaker.registry.ProfileCommands; // Import
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.item.ItemStack;
 
 public class SkyblockMakerMod implements ModInitializer {
     @Override
     public void onInitialize() {
         ModPackets.registerServerPackets();
-        
-        // Auto Converter Loop
-        ServerTickEvents.END_WORLD_TICK.register(world -> {
-            world.getPlayers().forEach(player -> {
-                for (int i = 0; i < player.getInventory().size(); i++) {
-                    ItemStack stack = player.getInventory().getStack(i);
-                    if (!stack.isEmpty()) {
-                        SkyblockItemDefaults.convertToSkyblockItem(stack);
-                    }
-                }
-            });
+
+        // Register Admin Commands
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            ProfileCommands.register(dispatcher);
         });
-        System.out.println("Hypixel Skyblock Engine Started!");
+        
+        // Stats Engine
+        ServerTickEvents.END_WORLD_TICK.register(world -> {
+            world.getPlayers().forEach(SkyblockStatHandler::updatePlayerStats);
+        });
     }
 }
