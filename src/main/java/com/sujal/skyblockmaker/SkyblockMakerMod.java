de78f14ad79bc9eff74f121a9ab135da806c9463
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-03 14:56:37 +0530

    Update SkyblockMakerMod.java

--- src/main/java/com/sujal/skyblockmaker/SkyblockMakerMod.java
+++ src/main/java/com/sujal/skyblockmaker/SkyblockMakerMod.java
@@ -1,25 +1,14 @@
 package com.sujal.skyblockmaker;
 
-import com.sujal.skyblockmaker.api.SkyblockStatHandler;
-import com.sujal.skyblockmaker.registry.ModPackets;
-import com.sujal.skyblockmaker.registry.ProfileCommands; // Import
+import com.sujal.skyblockmaker.registry.ModRegistries;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 
 public class SkyblockMakerMod implements ModInitializer {
     @Override
     public void onInitialize() {
-        ModPackets.registerServerPackets();
-
-        // Register Admin Commands
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            ProfileCommands.register(dispatcher);
-        });
+        // Sirf ek line ka call, baaki sab registry file mein
+        ModRegistries.registerModStuff();
         
-        // Stats Engine
-        ServerTickEvents.END_WORLD_TICK.register(world -> {
-            world.getPlayers().forEach(SkyblockStatHandler::updatePlayerStats);
-        });
+        System.out.println("SkyblockMaker Server Initialized!");
     }
 }
