1a23efaa42f3ae4af3648cc755a041a192f9ac57
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-03 10:18:04 +0530

    Update ItemBuilderScreen.java

--- src/main/java/com/sujal/skyblockmaker/client/gui/ItemBuilderScreen.java
+++ src/main/java/com/sujal/skyblockmaker/client/gui/ItemBuilderScreen.java
@@ -1,6 +1,6 @@
 package com.sujal.skyblockmaker.client.gui;
 
-import com.sujal.skyblockmaker.SkyblockMakerMod;
+import com.sujal.skyblockmaker.registry.ModPackets; // Import new registry
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.gui.DrawContext;
@@ -25,26 +25,21 @@ public class ItemBuilderScreen extends Screen {
         int centerX = this.width / 2;
         int y = 50;
 
-        // Name Field
         addDrawableChild(nameField = new TextFieldWidget(textRenderer, centerX - 100, y, 200, 20, Text.literal("Name")));
         nameField.setPlaceholder(Text.literal("Item Name"));
 
-        // Strength Field
         addDrawableChild(strengthField = new TextFieldWidget(textRenderer, centerX - 100, y + 30, 200, 20, Text.literal("Strength")));
-        strengthField.setPlaceholder(Text.literal("Strength (Number)"));
+        strengthField.setPlaceholder(Text.literal("Strength"));
 
-        // Rarity Field
         addDrawableChild(rarityField = new TextFieldWidget(textRenderer, centerX - 100, y + 60, 200, 20, Text.literal("Rarity")));
-        rarityField.setPlaceholder(Text.literal("Rarity (COMMON, RARE...)"));
+        rarityField.setPlaceholder(Text.literal("Rarity"));
 
-        // Create Button
         addDrawableChild(ButtonWidget.builder(Text.literal("Create"), button -> sendCreatePacket())
                 .dimensions(centerX - 50, y + 100, 100, 20)
                 .build());
     }
 
     private void sendCreatePacket() {
-        // Data prepare karo bhejne ke liye
         String name = nameField.getText();
         String rarity = rarityField.getText();
         double strength = 0;
@@ -52,16 +47,14 @@ public class ItemBuilderScreen extends Screen {
             strength = Double.parseDouble(strengthField.getText());
         } catch (Exception ignored) {}
 
-        // Packet Banao
         PacketByteBuf buf = PacketByteBufs.create();
         buf.writeString(name);
         buf.writeDouble(strength);
         buf.writeString(rarity);
 
-        // Server ko bhejo (Even in Singleplayer)
-        ClientPlayNetworking.send(SkyblockMakerMod.ITEM_CREATE_PACKET, buf);
-        
-        this.close(); // Close GUI
+        // Uses ModPackets ID
+        ClientPlayNetworking.send(ModPackets.ITEM_CREATE_PACKET, buf);
+        this.close();
     }
 
     @Override
