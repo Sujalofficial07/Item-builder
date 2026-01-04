04dcb61db2a03ed34b8aa5274bfc9122ad1bb7e6
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-04 11:56:05 +0530

    Update ModItems.java

--- src/main/java/com/sujal/skyblockmaker/registry/ModItems.java
+++ src/main/java/com/sujal/skyblockmaker/registry/ModItems.java
@@ -2,7 +2,6 @@ package com.sujal.skyblockmaker.registry;
 
 import com.sujal.skyblockmaker.api.SkyblockItem;
 import net.minecraft.item.Items;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,45 +10,57 @@ public class ModItems {
     public static final List<SkyblockItem> ITEMS = new ArrayList<>();
 
     public static void registerItems() {
-        // --- SWORDS ---
+        // --- DUNGEON WEAPONS ---
         SkyblockItem hyperion = new SkyblockItem("HYPERION", "Hyperion", Items.IRON_SWORD, "LEGENDARY", "SWORD");
-        hyperion.damage = 260;
-        hyperion.strength = 150;
-        hyperion.intelligence = 350;
-        hyperion.ferocity = 30;
+        hyperion.damage = 260; hyperion.strength = 150; hyperion.intelligence = 350; hyperion.ferocity = 30;
         hyperion.isDungeon = true;
         hyperion.abilityName = "Wither Impact";
-        hyperion.abilityDesc = "Teleports 10 blocks ahead and deals damage to nearby enemies.";
+        hyperion.abilityDesc = "Teleports 10 blocks ahead and deals 10,000 damage.";
         hyperion.manaCost = 250;
         ITEMS.add(hyperion);
 
-        SkyblockItem aotd = new SkyblockItem("AOTD", "Aspect of the Dragons", Items.DIAMOND_SWORD, "LEGENDARY", "SWORD");
-        aotd.damage = 225;
-        aotd.strength = 100;
-        aotd.abilityName = "Dragon Rage";
-        aotd.abilityDesc = "Deals 12,000 damage to mobs in front of you.";
-        aotd.manaCost = 100;
-        ITEMS.add(aotd);
+        SkyblockItem lividDagger = new SkyblockItem("LIVID_DAGGER", "Livid Dagger", Items.IRON_SWORD, "LEGENDARY", "SWORD");
+        lividDagger.damage = 210; lividDagger.strength = 60; lividDagger.critChance = 100; lividDagger.attackSpeed = 100;
+        lividDagger.isDungeon = true;
+        lividDagger.abilityName = "Throw";
+        lividDagger.abilityDesc = "Throw your dagger to deal damage!";
+        lividDagger.manaCost = 150;
+        ITEMS.add(lividDagger);
 
-        // --- BOWS ---
-        SkyblockItem terminator = new SkyblockItem("TERMINATOR", "Terminator", Items.BOW, "LEGENDARY", "BOW");
-        terminator.damage = 310;
-        terminator.strength = 50;
-        terminator.critDamage = 250;
-        terminator.attackSpeed = 40;
-        terminator.isDungeon = true;
-        terminator.abilityName = "Salvation";
-        terminator.abilityDesc = "Shoots 3 arrows at once. Can hit endermen.";
-        ITEMS.add(terminator);
+        SkyblockItem juju = new SkyblockItem("JUJU_SHORTBOW", "Juju Shortbow", Items.BOW, "EPIC", "BOW");
+        juju.damage = 310; juju.strength = 40; juju.critChance = 10; juju.critDamage = 110;
+        juju.isDungeon = true;
+        juju.abilityDesc = "Hits 3 mobs instantly.";
+        ITEMS.add(juju);
 
-        // --- MATERIALS ---
-        SkyblockItem enchCobble = new SkyblockItem("ENCHANTED_COBBLESTONE", "Enchanted Cobblestone", Items.COBBLESTONE, "COMMON", "MATERIAL");
-        enchCobble.isEnchanted = true; // Needs glow logic (usually adding Unbreaking I + hide flags)
-        ITEMS.add(enchCobble);
+        // --- DUNGEON ARMOR (Necron) ---
+        addArmorSet("NECRON", "Necron's", "LEGENDARY", 40, 250, 80, 30);
+        addArmorSet("STORM", "Storm's", "LEGENDARY", 20, 150, 40, 250); // High Intel
+        addArmorSet("SHADOW_ASSASSIN", "Shadow Assassin", "EPIC", 30, 200, 50, 10);
+
+        // --- MATERIALS (Enchanted) ---
+        addMaterial("ENCHANTED_COBBLESTONE", "Enchanted Cobblestone", Items.COBBLESTONE, "COMMON");
+        addMaterial("ENCHANTED_OBSIDIAN", "Enchanted Obsidian", Items.OBSIDIAN, "UNCOMMON");
+        addMaterial("ENCHANTED_DIAMOND_BLOCK", "Enchanted Diamond Block", Items.DIAMOND_BLOCK, "RARE");
+        addMaterial("NECRON_HANDLE", "Necron's Handle", Items.STICK, "LEGENDARY");
+    }
+
+    private static void addArmorSet(String idBase, String nameBase, String rarity, double str, double def, double cd, double intel) {
+        // Helmet
+        SkyblockItem helm = new SkyblockItem(idBase + "_HELMET", nameBase + " Helmet", Items.PLAYER_HEAD, rarity, "ARMOR");
+        helm.strength = str; helm.defense = def; helm.critDamage = cd; helm.intelligence = intel; helm.health = 100; helm.isDungeon = true;
+        ITEMS.add(helm);
         
-        SkyblockItem enchDiamond = new SkyblockItem("ENCHANTED_DIAMOND", "Enchanted Diamond", Items.DIAMOND, "RARE", "MATERIAL");
-        enchDiamond.isEnchanted = true;
-        ITEMS.add(enchDiamond);
+        // Chestplate
+        SkyblockItem chest = new SkyblockItem(idBase + "_CHESTPLATE", nameBase + " Chestplate", Items.LEATHER_CHESTPLATE, rarity, "ARMOR");
+        chest.strength = str+10; chest.defense = def+40; chest.critDamage = cd; chest.intelligence = intel; chest.health = 160; chest.isDungeon = true;
+        ITEMS.add(chest);
+    }
+
+    private static void addMaterial(String id, String name, net.minecraft.item.Item mat, String rarity) {
+        SkyblockItem item = new SkyblockItem(id, name, mat, rarity, "MATERIAL");
+        item.isEnchanted = true;
+        ITEMS.add(item);
     }
 
     public static List<SkyblockItem> getByCategory(String category) {
